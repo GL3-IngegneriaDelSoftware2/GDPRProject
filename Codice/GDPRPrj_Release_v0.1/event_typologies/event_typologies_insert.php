7ace71573cd4c9f37e4c57a6f6bbe8a763f91e70
LucaPellizzari96 <lucapellizzari96@libero.it>
2019-02-24 21:15:08 +0100

    Fix minori ai form

--- Codice/GDPRPrj_Release_v0.1/event_typologies/event_typologies_insert.php
+++ Codice/GDPRPrj_Release_v0.1/event_typologies/event_typologies_insert.php
@@ -7,6 +7,7 @@ $name = $_POST['name']; // Stringa nome tipologia dell'evento
 $priority = $_POST['priority']; // Priorita' evento, stringa di un interno da 1 a 5 compresi
 $early_notification = $_POST['early_notification']; // Notifica anticipata, stringa anel formato ("hh");
 $repeat_interval = $_POST['repeat_interval']; // Stringa che indica l'intervallo di ripetizione
+$typology_color = $_POST['typology_color']; // colore legato alla tipologia di evento
 
 $early_notification_hours = (int)$early_notification; // Rendo la variabile un intero
 
@@ -17,7 +18,7 @@ $dbPassword = "";
 $dbname = "gdpr_database";
 $tableName = "event_typologies";
 
-if (!empty($name) || !empty($priority) || !empty($early_notification) || !empty($repeat_interval) || !empty($class) || !empty($state)) {
+if (!empty($name) || !empty($priority) || !empty($early_notification) || !empty($repeat_interval)) {
 	
     // create connection
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
@@ -25,10 +26,10 @@ if (!empty($name) || !empty($priority) || !empty($early_notification) || !empty(
     if (mysqli_connect_error()) { // connessione fallita
      die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
     }else{ // connessione ha successo
-      $INSERT = "insert into $tableName (et_name, et_priority, et_early_notification, et_event_repeat) values(?, ?, ?, ?)";
+      $INSERT = "insert into $tableName (et_name, et_priority, et_early_notification, et_event_repeat, et_color) values(?, ?, ?, ?, ?)";
       // Prepare statement
       $stmt = $conn->prepare($INSERT); // Prepariamo per INSERT
-      $stmt->bind_param('siii', $name, $priority, $early_notification_hours, $repeat_interval); // $dateFrom, $dateTo, $class, $state, $notes, $participants, $actualStart, $actualEnd);
+      $stmt->bind_param('siiss', $name, $priority, $early_notification_hours, $repeat_interval, $typology_color);
       $stmt->execute();
       $stmt->close();
       $conn->close();
