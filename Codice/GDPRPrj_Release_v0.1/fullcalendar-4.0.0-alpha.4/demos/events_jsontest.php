6763d5a669b24feecd1c1064d057784316a7574a
Luca Pellizzari <lucapellizzari96@libero.it>
2019-03-21 10:34:20 +0100

    Aggiunti screen, manuale e ripetiz eventi

--- Codice/GDPRPrj_Release_v0.1/fullcalendar-4.0.0-alpha.4/demos/events_jsontest.php
+++ Codice/GDPRPrj_Release_v0.1/fullcalendar-4.0.0-alpha.4/demos/events_jsontest.php
@@ -22,7 +22,7 @@ if (mysqli_connect_errno()) {
 }
 
 // Estraggo i dati dalla tabella
-$query = "select $tableName.e_name, $tableName.e_date_from, $tableName.e_date_to, et.et_color from $tableName join event_typologies et on $tableName.e_typology = et.et_id";
+$query = "select events.e_name, events.e_date_from, events.e_date_to, et.et_color, et.et_event_repeat from events join event_typologies et on events.e_typology = et.et_id";
 $dbResult = mysqli_query($link, $query); // risultato della query
 $eventsArray = array(); // array di array di eventi
 
@@ -49,21 +49,51 @@ mysqli_close($link);
 	// pass PHP array to JavaScript 
 	var eventsTemp = <?php echo json_encode($eventsArray, JSON_PRETTY_PRINT) ?>;
 	
-	function Event(title, start, end, color){
-		this.title = title;
-		this.start = start;
-		this.end = end;
-		this.color = color;
+	// COSTRUTTORE dell'oggetto Event
+	function Event(title, start, end, color, flagRepetition, repetitionDay){
+		if(flagRepetition){ // se l'evento si ripete uso startRecur e endRecur invece di start e end
+			this.title = title;
+			this.color = color;
+			// RIPETIZIONE:
+			this.startRecur = start; // con startRecur il campo this.start non serve
+			this.endRecur = end; // con endRecur il campo this.end non serve
+			this.daysOfWeek = repetitionDay; // un array di interi con 0 = Sunday, 1 = Monday,..., se non presente l'evento si ripete ogni giorno
+		}else{ // se l'evento non si ripete uso il costruttore normale
+			this.title = title;
+			this.start = start;
+			this.end = end;
+			this.color = color;
+		}
 	}
 	
 	var events = new Array();
 	
-	for(var i = 0; i < eventsTemp.length; i+=4){
-		events.push(new Event(eventsTemp[i], eventsTemp[i+1], eventsTemp[i+2], eventsTemp[i+3]));
-	}
+	for(var i = 0; i < eventsTemp.length; i+=5){
+		var eventRepetition = eventsTemp[i+4]; // campo et.et_event_repeat della tabella event_typologies
+		switch(eventRepetition) {
+		  case "daily":
+			events.push(new Event(eventsTemp[i], eventsTemp[i+1], eventsTemp[i+2], eventsTemp[i+3], true));
+			break;
+		  case "weekly":
+			var d = new Date(eventsTemp[i+1]); // data di inizio dell'evento
+			var a = new Array();
+			a.push(d.getDay()); // a = array con all'interno il numero (0-6) del giorno della settimana in cui si ripete l'evento
+			events.push(new Event(eventsTemp[i], eventsTemp[i+1], eventsTemp[i+2], eventsTemp[i+3], true, a));
+			break;
+		  case "monthly":
+			events.push(new Event(eventsTemp[i], eventsTemp[i+1], eventsTemp[i+2], eventsTemp[i+3], false, [])); // TEMP come se fosse don't repeat
+			break;
+		  case "yearly":
+			events.push(new Event(eventsTemp[i], eventsTemp[i+1], eventsTemp[i+2], eventsTemp[i+3], false, [])); // TEMP come se fosse don't repeat
+			break;
+		  default: // "don't repeat"
+			events.push(new Event(eventsTemp[i], eventsTemp[i+1], eventsTemp[i+2], eventsTemp[i+3], false, []));
+			break;
+		} // switch
+	} // for
 
 	// how to access 
-	console.log( events );
+	//console.log( events );
 </script>
 
 <!DOCTYPE html>
